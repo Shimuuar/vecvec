b86bc5bfbac99119f72ae7752fdaa7bffdd86f95
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2010-01-16 23:15:24 +0300

    * * Add new data new to encapsulate 1D boosts. Removes much of worries about   misunderstaning of first parameter in boost

--- Data/VectorSpace/Lorentz.hs
+++ Data/VectorSpace/Lorentz.hs
@@ -1,20 +1,44 @@
 {-# LANGUAGE TypeFamilies #-}
-module Data.VectorSpace.Lorentz ( Lorentz(..)
+module Data.VectorSpace.Lorentz ( -- * Lorentz vector
+                                  Lorentz(..)
+                                -- ** Convert between rapidity, gamma factor & speed
+                                , vToRapidity
                                 , vToGamma
+                                , rapidityToGamma
+                                , rapidityToV
                                 , gammaToV
+                                , gammaToRapidity
                                 -- * Boosts 
+                                -- ** One dimesional boosts
+                                , Boost1D
+                                -- *** Constructors & deconstructors
+                                , rapidityBoost1D
+                                , boost1Drapidity
+                                , gammaBoost1D
+                                , boost1Dgamma
+                                , vBoost1D
+                                , boost1Dv
+                                -- *** Perform boosts on lorentz vectors
                                 , boostX
                                 , boostY
                                 , boostZ
-                                , boost
                                 ) where
 
+import Data.Monoid
 import Data.VectorSpace
 import Data.VectorSpace.LowDim
 
+----------------------------------------------------------------
+-- Lorentz vectors
+----------------------------------------------------------------
+
 -- | Lorentz vector
-data Lorentz a = Lorentz a a a a
-                 deriving (Eq, Show)
+data Lorentz a = Lorentz { lorentzT :: a
+                         , lorentzX :: a
+                         , lorentzY :: a
+                         , lorentzZ :: a
+                         }
+                 deriving (Eq, Show, Read)
 
 instance Num a => AdditiveGroup (Lorentz a) where
     zeroV = Lorentz 0 0 0 0
@@ -29,29 +53,82 @@ instance Num a => InnerSpace (Lorentz a) where
     (Lorentz t1 x1 y1 z1) <.> (Lorentz t2 x2 y2 z2) = t1*t2 - x1*x2 - y1*y2 - z1*z2
 
 
+-- | Convert speed to rapidity
+vToRapidity :: Floating a => a -> a
+vToRapidity = atanh
+
 -- | Convert speed in speed of light units to gamma factor
 vToGamma :: Floating a => a -> a
 vToGamma v = 1 / sqrt( 1 - v**2 )
+
+-- | Convert rapidity to gamma factor
+rapidityToGamma :: Floating a => a -> a
+rapidityToGamma = cosh
+
+-- | Convert rapidity to speed
+rapidityToV :: Floating a => a -> a
+rapidityToV = tanh
 
 -- | Convert gamma factor to speed 
 gammaToV :: Floating a => a -> a
 gammaToV g = sqrt( (g**2 - 1)/ g**2 )
 
--- | boost along X axis
-boostX :: Floating a => a -> Lorentz a -> Lorentz a
-boostX gamma (Lorentz t x y z) = Lorentz (gamma*(t + v*x)) (gamma*(x + v*t)) y z
-    where v = gammaToV gamma
+-- | Convert gamma factor to rapidity
+gammaToRapidity :: Floating a => a -> a
+gammaToRapidity = acosh
 
--- | boost againist Y axis
-boostY :: Floating a => a -> Lorentz a -> Lorentz a
-boostY gamma (Lorentz t x y z) = Lorentz (gamma*(t + v*y)) x (gamma*(y + v*t)) z
-    where v = gammaToV gamma
+
+----------------------------------------------------------------
+-- 1D Boosts
+----------------------------------------------------------------
+
+-- | One dimensinal boost. It deserve specian treatment since it forms
+--   a group.
+--
+--   It's parametrized by rapidity.
+newtype Boost1D a = Boost1D a
+                    deriving (Eq,Show,Read)
+
+instance Floating a => Monoid (Boost1D a) where
+    mempty = Boost1D 0
+    mappend (Boost1D x) (Boost1D y) = Boost1D (x + y)
+
+
+rapidityBoost1D :: Floating a => a -> Boost1D a
+rapidityBoost1D = Boost1D
+
+boost1Drapidity :: Floating a => Boost1D a -> a
+boost1Drapidity (Boost1D x) = x
+
+gammaBoost1D :: Floating a => a -> Boost1D a
+gammaBoost1D = Boost1D . gammaToRapidity
+
+boost1Dgamma :: Floating a => Boost1D a -> a
+boost1Dgamma (Boost1D x) = rapidityToGamma x
+
+vBoost1D :: Floating a => a -> Boost1D a 
+vBoost1D = Boost1D . vToRapidity
+
+boost1Dv :: Floating a => Boost1D a -> a
+boost1Dv (Boost1D x) = rapidityToV x
+
+-- | Boost along X axis
+boostX :: Floating a => Boost1D a -> Lorentz a -> Lorentz a
+boostX (Boost1D φ) (Lorentz t x y z) = Lorentz (cosh φ * t + sinh φ * x) (sinh φ * t + cosh φ * x) y z
+
+-- | Boost againist Y axis
+boostY :: Floating a => Boost1D a -> Lorentz a -> Lorentz a
+boostY (Boost1D φ) (Lorentz t x y z) = Lorentz (cosh φ * t + sinh φ * y) x (sinh φ * t + cosh φ * y) z 
 
 -- | Boost againist Z axis 
-boostZ :: Floating a => a -> Lorentz a -> Lorentz a
-boostZ gamma (Lorentz t x y z) = Lorentz (gamma*(t + v*z)) x y (gamma*(z + v*t)) 
-    where v = gammaToV gamma
+boostZ :: Floating a => Boost1D a -> Lorentz a -> Lorentz a
+boostZ (Boost1D φ) (Lorentz t x y z) = Lorentz (cosh φ * t + sinh φ * z) x y (sinh φ * t + cosh φ * z)
 
+----------------------------------------------------------------
+-- Generi boosts
+----------------------------------------------------------------
+
+{-
 boost :: Floating a => Vec3D a -> Lorentz a -> Lorentz a 
 boost l (Lorentz t x y z) =
     let gamma  = magnitude l
@@ -65,3 +142,8 @@ boost l (Lorentz t x y z) =
         z' = gamma*v*nz*t + gamma1*nx*nz*x       + gamma1*ny*nz*y       + (gamma1*nz*nz + 1)*z
     in Lorentz t' x' y' z'
 
+boostRap :: Floating a => Vec3D a -> Lorentz a -> Lorentz a  
+boostRap rap = let r = magnitude rap
+                   g = rapidityToGamma r
+               in boost ((g/r) *^ rap)
+-}
