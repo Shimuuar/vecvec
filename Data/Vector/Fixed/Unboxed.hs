8a044b9b19130c0e76b07093106e077313053127
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-10-10 23:17:53 +0400

    Actually working unboxed version.
    No sure about efficiency though

--- Data/Vector/Fixed/Unboxed.hs
+++ Data/Vector/Fixed/Unboxed.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-
 {-# LANGUAGE ImpredicativeTypes #-}
 -- |
 -- Unboxed vectors with fixed length
@@ -10,9 +9,11 @@ module Data.Vector.Fixed.Unboxed (
     Vec
   ) where
 
+import Control.Monad
 import Control.Monad.ST
 import Data.Primitive.ByteArray
 import Data.Primitive
+import Prelude hiding (length)
 
 import Data.Vector.Fixed
 
@@ -27,8 +28,8 @@ data Vec n a = Vec {-# UNPACK #-} !Int       -- Offset from start
 
 type instance Dim (Vec n) = n
 
-instance (Arity n, Prim a) => Vector (Vec n) a where
-  construct = undefined
+instance (Arity n, Prim a, Nat n) => Vector (Vec n) a where
+  construct = fmap makeVec construct
   inspect   = inspectVec
 
 -- Deconstruct vector
@@ -38,43 +39,20 @@ inspectVec v (Fun f)
           (T_idx 0 `asVec` v)
           f
 
--- Create vector
-constructVec :: forall s n a. (Arity n, Prim a) => Fun n a (ST s (Vec n a))
-constructVec
-  = Fun $ accumM
-          step
-          (fini
-             :: T_alloc s a Z -> ST s (Vec n a)
-          )
-          (fresh :: ST s (T_alloc s a n))
-
-step :: Prim a => T_alloc s a (S k) -> a -> ST s (T_alloc s a k)
-step (T_alloc i arr) x = do
-  writeByteArray arr i x
-  return $! T_alloc (i+1) arr
-
-fini :: Prim a => T_alloc s a Z -> ST s (Vec n a)
-fini (T_alloc _ arr) = do
-  xs <- unsafeFreezeByteArray arr
-  return $! Vec 0 xs
-
-fresh :: ST s (T_alloc s a n)
-fresh = do
-  arr <- newByteArray 0
-  return $ T_alloc 0 arr
+-- It's downright impossible to write construct for Vec using
+-- accum. runST require existential quantification and mess everything
+-- up!
+makeVec :: forall n a. (Arity n, Prim a, Nat n) => VecList n a -> Vec n a
+makeVec v@(VecList xs) = runST $ do
+  arr <- newByteArray $! n * sizeOf (undefined :: a)
+  forM_ (zip [0..] xs) $ \(i,x) -> do
+    writeByteArray arr i x
+  vec <- unsafeFreezeByteArray arr
+  return $ Vec 0 vec
+  where
+    n = length v
 
 
-funST :: forall n a b. (Arity n) => Fun n a (forall s. ST s b) -> Fun n a b
-funST f = fmap runST f
-
-
-
-newtype BoxST a = BoxST (forall s. ST s a)
-
-chain :: BoxST a -> BoxST b -> BoxST b
-chain (BoxST a) (BoxST b) = BoxST $ a >> b
-
-data T_alloc s a n = T_alloc Int (MutableByteArray s)
 
 ----------------------------------------------------------------
 -- Helpers
