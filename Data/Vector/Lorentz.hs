8dc5c2e43e42db0686dc3bfd4023c29d0890b65b
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2013-02-25 17:49:26 +0400

    More generic instance for Boost1D

--- Data/Vector/Lorentz.hs
+++ Data/Vector/Lorentz.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE UndecidableInstances  #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ViewPatterns #-}
 -- |
 -- Unboxed Lorentz vectors
 module Data.Vector.Lorentz (
@@ -28,25 +29,26 @@ import Data.Monoid    (Monoid(..))
 import Data.Classes.AdditiveGroup
 import Data.Classes.VectorSpace
 
-import Data.Vector.Fixed         as F hiding (convert)
-import Data.Vector.Fixed.Unboxed
+import           Data.Vector.Fixed (Vector,Dim,S,N2,N3)
+import qualified Data.Vector.Fixed as F
+import Data.Vector.Fixed.Unboxed   (Vec,Unbox)
 
 
 ----------------------------------------------------------------
 -- Data type
 ----------------------------------------------------------------
 
--- | Generic n-dimensional Lorentz vector
+-- | Lorentz vector with /n/-dimensional spatial part.
 newtype LorentzN n a = Lorentz (Vec (S n) a)
 
 -- | Normal 4-dimensional Lorentz vector
 type Lorentz = LorentzN N3
 
-type instance Dim  (LorentzN n) = S n
+type instance Dim (LorentzN n) = S n
 
 instance (Unbox (S n) a) => Vector (LorentzN n) a where
-  construct             = fmap Lorentz construct
-  inspect (Lorentz v) f = inspect v f
+  construct             = fmap Lorentz F.construct
+  inspect (Lorentz v) f = F.inspect v f
 
 -- | Spatial part of the Lorentz vector.
 spatialPart :: (Unbox n a, Unbox (S n) a) => LorentzN n a -> Vec n a
@@ -80,13 +82,13 @@ class Convert a b where
   convert :: a -> b
 
 instance Convert Speed Gamma where
-  convert (Speed v) = Gamma $ 1 / sqrt (1 - v*v)
+  convert (Speed v)    = Gamma $ 1 / sqrt (1 - v*v)
 instance Convert Speed Rapidity where
-  convert (Speed v) = Rapidity $ atanh v
+  convert (Speed v)    = Rapidity $ atanh v
 instance Convert Gamma Speed where
-  convert (Gamma γ) = Speed $ sqrt $ (g2 -1) / g2 where g2 = γ*γ
+  convert (Gamma γ)    = Speed $ sqrt $ (g2 -1) / g2 where g2 = γ*γ
 instance Convert Gamma Rapidity where
-  convert (Gamma γ) = Rapidity $ acosh γ
+  convert (Gamma γ)    = Rapidity $ acosh γ
 instance Convert Rapidity Speed where
   convert (Rapidity φ) = Speed $ tanh φ
 instance Convert Rapidity Gamma where
@@ -94,17 +96,28 @@ instance Convert Rapidity Gamma where
 
 -- | Boost for 1+1 space.
 class Boost1D a where
-  boost1D :: a                  -- ^ Boost parameter
-          -> (Double,Double)    -- ^ (time,coordinate)
-          -> (Double,Double)
+  boost1D :: (Vector v Double, Dim v ~ N2)
+          =>  a                  -- ^ Boost parameter
+          -> v Double -> v Double
+
+instance Boost1D Speed where
+  boost1D (Speed v) (F.convert -> (t,x))
+    = F.mk2 (γ*(t + v*x))
+            (γ*(v*t + x))
+    where
+      Gamma γ = convert (Speed v)
 
 instance Boost1D Gamma where
-  boost1D (Gamma γ) (t,x)
-    = (γ*(t + v*x), γ*(v*t + x)) where Speed v = convert (Gamma γ)
+  boost1D (Gamma γ) (F.convert -> (t,x))
+    = F.mk2 (γ*(t + v*x))
+            (γ*(v*t + x))
+    where
+      Speed v = convert (Gamma γ)
 
 instance Boost1D Rapidity where
-  boost1D (Rapidity φ) (t,x)
-    = (c*t + s*x, s*t + c*x)
+  boost1D (Rapidity φ) (F.convert -> (t,x))
+    = F.mk2 (c*t + s*x)
+            (s*t + c*x)
     where
       c = cosh φ
       s = sinh φ
@@ -118,27 +131,27 @@ instance Boost1D Rapidity where
 type instance Scalar (LorentzN n a) = a
 
 instance (Unbox (S n) a, Num a) => AdditiveMonoid (LorentzN n a) where
-  zeroV = replicate 0
-  (.+.) = zipWith (+)
+  zeroV = F.replicate 0
+  (.+.) = F.zipWith (+)
   {-# INLINE zeroV #-}
   {-# INLINE (.+.) #-}
 
 instance (Unbox (S n) a, Num a) => AdditiveGroup (LorentzN n a) where
-  negateV = map negate
-  (.-.)   = zipWith (-)
+  negateV = F.map negate
+  (.-.)   = F.zipWith (-)
   {-# INLINE negateV #-}
   {-# INLINE (.-.)   #-}
 
 instance (Unbox (S n) a, Num a) => LeftModule  (LorentzN n a) where
-  a *. v = map (a *) v
+  a *. v = F.map (a *) v
   {-# INLINE (*.) #-}
 
 instance (Unbox (S n) a, Num a) => RightModule (LorentzN n a) where
-  v .* a = map (* a) v
+  v .* a = F.map (* a) v
   {-# INLINE (.*) #-}
 
 instance (Unbox (S n) a, Num a) => InnerSpace (LorentzN n a) where
-  v <.> u = sum $ izipWith minkovsky v u
+  v <.> u = F.sum $ F.izipWith minkovsky v u
     where
       minkovsky 0 x y =   x*y
       minkovsky _ x y = -(x*y)
