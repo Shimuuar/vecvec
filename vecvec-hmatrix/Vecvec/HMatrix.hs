5bab8ec9191fb574d487da561d5a9231e02bae40
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2023-04-11 16:20:43 +0300

    Add more instances for hmatrix
    There's a _lot_ of instances involving Tr/Conj. I'm not sure whether I like this
    9x blowup.

--- vecvec-hmatrix/Vecvec/HMatrix.hs
+++ vecvec-hmatrix/Vecvec/HMatrix.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE ImportQualifiedPost   #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies          #-}
@@ -12,7 +13,6 @@ import Numeric.LinearAlgebra.Devel
 import Vecvec.Classes
 
 import Data.Vector.Storable qualified as VS
--- FIXME: Avoid weirdness
 
 instance (Num a, Element a) => AdditiveSemigroup (Matrix a) where
   (.+.) = liftMatrix2 (VS.zipWith (+))
@@ -26,8 +26,57 @@ instance (Num a, Element a) => VectorSpace (Matrix a) where
   a *. m = liftMatrix (VS.map (a*)) m
   m .* a = liftMatrix (VS.map (*a)) m
 
-instance (a ~ b, a ~ c, Num a, Numeric a) => MatMul (Matrix a) (Matrix b) (Matrix c) where
+-- NOTE: Here we hope that tr' and our NormedScalar are compatible. What to do about it?
+
+-- FIXME: Do we really have to conjugate whole matrix???
+
+----------------------------------------------------------------
+-- Matrix-matrix
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Matrix a) (Matrix b) (Matrix a) where
   (@@) = (HM.<>)
 
-instance (a ~ b, a ~ c, Num a, Numeric a) => MatMul (Matrix a) (Vector b) (Vector c) where
+instance (a ~ b, Num a, Numeric a) => MatMul (Tr (Matrix a)) (Matrix b) (Matrix a) where
+  Tr a @@ b = tr a HM.<> b
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Conj (Matrix a)) (Matrix b) (Matrix a) where
+  Conj a @@ b = tr' a HM.<> b
+
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Matrix a) (Tr (Matrix b)) (Matrix a) where
+  a @@ Tr b = a HM.<> tr b
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Tr (Matrix a)) (Tr (Matrix b)) (Matrix a) where
+  Tr a @@ Tr b = tr a HM.<> tr b
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Conj (Matrix a)) (Tr (Matrix b)) (Matrix a) where
+  Conj a @@ Tr b = tr' a HM.<> tr b
+
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Matrix a) (Conj (Matrix b)) (Matrix a) where
+  a @@ Conj b = a HM.<> tr' b
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Tr (Matrix a)) (Conj (Matrix b)) (Matrix a) where
+  Tr a @@ Conj b = tr a HM.<> tr' b
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Conj (Matrix a)) (Conj (Matrix b)) (Matrix a) where
+  Conj a @@ Conj b = tr' a HM.<> tr' b
+
+
+----------------------------------------------------------------
+-- Matrix-vector
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Matrix a) (Vector b) (Vector a) where
   (@@) = (HM.#>)
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Tr (Matrix a)) (Vector b) (Vector a) where
+  Tr m @@ v = tr m HM.#> v
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Conj (Matrix a)) (Vector b) (Vector a) where
+  Conj m @@ v = tr' m HM.#> v
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Tr (Vector a)) (Matrix b) (Tr (Vector a)) where
+  Tr v @@ m = Tr (tr m #> v)
+
+instance (a ~ b, Num a, Numeric a) => MatMul (Conj (Vector a)) (Matrix b) (Conj (Vector a)) where
+  Conj v @@ m = Conj (tr' m #> v)
