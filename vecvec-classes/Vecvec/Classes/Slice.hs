01ee452625799ac66ae6151b6a2e83e4a9dd1c00
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2023-05-06 21:34:58 +0300

    Document API and add instances for mutable vectors
    Also fix one bug. But we still need tests. Badly

--- vecvec-classes/Vecvec/Classes/Slice.hs
+++ vecvec-classes/Vecvec/Classes/Slice.hs
@@ -5,19 +5,42 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies          #-}
 -- |
-module Vecvec.Classes.Slice where
+module Vecvec.Classes.Slice
+  ( -- * Type classes
+    Slice(..)
+  , Slice1D(..)
+    -- ** Slice parameters
+  , Range(..)
+  , End(..)
+  , Length(..)
+    -- * Default implementations
+  , implSliceVector
+  , implSliceMVector
+  ) where
 
 import GHC.Generics (Generic)
-import Data.Vector           qualified as V
-import Data.Vector.Unboxed   qualified as VU
-import Data.Vector.Storable  qualified as VS
-import Data.Vector.Primitive qualified as VP
-import Data.Vector.Generic   qualified as VG
+import Data.Vector                   qualified as V
+import Data.Vector.Unboxed           qualified as VU
+import Data.Vector.Storable          qualified as VS
+import Data.Vector.Primitive         qualified as VP
+import Data.Vector.Generic           qualified as VG
+import Data.Vector.Mutable           qualified as MV
+import Data.Vector.Unboxed.Mutable   qualified as MVU
+import Data.Vector.Storable.Mutable  qualified as MVS
+import Data.Vector.Primitive.Mutable qualified as MVP
+import Data.Vector.Generic.Mutable   qualified as MVG
 
 
+
+-- | Very generic type class for slicing vectors, matrices etc. It's
+--   expected that slice is done in /O(1)/. There are many possible
+--   parameterizations of slice and different data structures require
+--   different parameterizations. This type class is used to abstract
+--   over this.
 class Slice i v where
+  -- | Expected /O(1)/. Return slice of vector @v@ or @Nothing@ if
+  --   required slice is not valid.
   sliceMaybe :: i -> v -> Maybe v
-
 
 -- | Type class for data types describing slice of dense one dimension
 --   vector with 0-based indices
@@ -55,7 +78,7 @@ instance (i ~ Int) => Slice1D (Range i) where
     | otherwise = Just (i, j-i)
     where
       mirror k | k > 0     = k
-               | otherwise = len - 1 - k
+               | otherwise = len + k
       i = mirror i0
       j = mirror j0
 
@@ -68,9 +91,11 @@ instance (i ~ Int) => Slice1D (Range i) where
 data Range a = a :.. a
   deriving stock (Show,Eq,Ord,Generic)
 
+-- | Used for creating slice to end of vector.
 data End = End
   deriving stock (Show,Eq,Ord,Generic)
 
+-- | Use length of vector for a slice.
 newtype Length = Length { unLength :: Int }
   deriving stock (Show,Eq,Ord,Generic)
 
@@ -94,10 +119,38 @@ instance (Slice1D idx, VU.Unbox a) => Slice idx (VU.Vector a) where
   sliceMaybe = implSliceVector
 
 
+instance (Slice1D idx) => Slice idx (MV.MVector s a) where
+  {-# INLINE sliceMaybe #-}
+  sliceMaybe = implSliceMVector
+
+instance (Slice1D idx, VP.Prim a) => Slice idx (MVP.MVector s a) where
+  {-# INLINE sliceMaybe #-}
+  sliceMaybe = implSliceMVector
+
+instance (Slice1D idx, VS.Storable a) => Slice idx (MVS.MVector s a) where
+  {-# INLINE sliceMaybe #-}
+  sliceMaybe = implSliceMVector
+
+instance (Slice1D idx, VU.Unbox a) => Slice idx (MVU.MVector s a) where
+  {-# INLINE sliceMaybe #-}
+  sliceMaybe = implSliceMVector
 
 
+
+----------------------------------------------------------------
+-- Default implementations
+----------------------------------------------------------------
+
+-- | Default implementation of 'sliceMaybe' in terms of 'Slice1D'.
 implSliceVector :: (Slice1D idx, VG.Vector v a) => idx -> v a -> Maybe (v a)
 {-# INLINE implSliceVector #-}
 implSliceVector idx vec = do
   (i,n) <- computeSlice1D (VG.length vec) idx
   Just $ VG.slice i n vec
+
+-- | Default implementation of 'sliceMaybe' in terms of 'Slice1D'.
+implSliceMVector :: (Slice1D idx, MVG.MVector v a) => idx -> v s a -> Maybe (v s a)
+{-# INLINE implSliceMVector #-}
+implSliceMVector idx vec = do
+  (i,n) <- computeSlice1D (MVG.length vec) idx
+  Just $ MVG.slice i n vec
