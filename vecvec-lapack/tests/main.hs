8bfba904709b44703e48bddbea07c44f2bc3b5e2
Dmitry Krylov <dima@krylov.ws>
2023-05-09 22:08:23 +1000

    Copy some property tests from `vector`

--- vecvec-lapack/tests/main.hs
+++ vecvec-lapack/tests/main.hs
@@ -1,10 +1,12 @@
 import Test.Tasty (defaultMain,testGroup)
 import qualified TST.Model
 import qualified TST.Slice
+import qualified TST.Property
 
 
 main :: IO ()
 main = defaultMain $ testGroup "vecvec"
   [ TST.Model.tests
   , TST.Slice.tests
+  , TST.Property.tests
   ]
