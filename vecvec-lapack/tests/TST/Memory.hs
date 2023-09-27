b5d2decb2c63676911df608e9f8747a2d0618b01
Dmitry Krylov <dima@krylov.ws>
2023-09-27 16:36:46 +1000

    test for intersected

--- vecvec-lapack/tests/TST/Memory.hs
+++ vecvec-lapack/tests/TST/Memory.hs
@@ -26,15 +26,21 @@ import Vecvec.LAPACK.Internal.Vector.Mutable (Strided(..))
 
 import Control.Monad.ST
 
+assertArrays :: (VS.Storable a, Eq a, Show a) => VV.Vec a -> [a] -> VV.Vec a -> [a] -> VV.Vec a -> [a] -> IO ()
+assertArrays givenV1 expectedV1 givenV2 expectedV2 givenV3 expectedV3 = do
+  assertEqual "V1" givenV1 (VG.fromList expectedV1)
+  assertEqual "V2" givenV2 (VG.fromList expectedV2)
+  assertEqual "V3" givenV3 (VG.fromList expectedV3)
+
+
 tests :: TestTree
 tests = testGroup "memory"
   [ testCase "simple" $ do
-        putStrLn "UPS"
         --
         -- TODO check also with **strided** vectors! -- via properties!!
         --
         -- let (vec :: VV.MVec MVG.RealWorld Int)  = runST $ MVG.basicUnsafeReplicate 10 123
-        let (vecOrig :: VV.Vec Int, vecSlice1 :: VV.Vec Int, vecSlice2 :: VV.Vec Int)  = runST $ do
+        let (vecCarrier :: VV.Vec Int, vecSlice1 :: VV.Vec Int, vecSlice2 :: VV.Vec Int)  = runST $ do
                     -- vec <- MVG.basicUnsafeReplicate 10 123
                     vec <- MVG.generateM 20 (pure . (+10)) -- [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
                     let v1 = MVG.slice 0 10 vec            -- [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
@@ -48,9 +54,31 @@ tests = testGroup "memory"
                     v1f  <- VG.freeze v1
                     v2f  <- VG.freeze v2
                     pure (vecf, v1f, v2f)
-        print vecOrig
-        print vecSlice1
-        print vecSlice2
-        pure ()
+
+        --print vecCarrier
+        --print vecSlice1
+        --print vecSlice2
+
+        assertArrays vecCarrier [10,11,12,13,14,10,11,12,13,14,15,16,17,18,19,25,26,27,28,29]
+                     vecSlice1  [10,11,12,13,14,10,11,12,13,14]
+                     vecSlice2  [10,11,12,13,14,15,16,17,18,19]
+  , testCase "intersected with stride" $ do
+        let (vecCarrier :: VV.Vec Int, vecSlice1 :: VV.Vec Int, vecSlice2 :: VV.Vec Int)  = runST $ do
+                    vec <- MVG.generateM 10 (pure . (+10)) -- [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+                    let vec1 = slice (Strided (0, End) 2) vec
+                    let vec2 = slice (2, Length 5) vec
+                    MVG.basicUnsafeMove vec2 {-target-} vec1 {-source-}
+                    (,,) <$> VG.freeze vec <*> VG.freeze vec1 <*> VG.freeze vec2
+
+        --print vecCarrier
+        --print vecSlice1
+        --print vecSlice2
+
+        --                      [10,11,12,13,14,15,16,17,18,19
+        assertArrays vecCarrier [10,11,10,12,14,16,18,17,18,19]
+                                    -- ^^^^^^^^^^^^^^ reinserted part
+                     vecSlice1  [10,   10,   14,   18,   18]
+                     vecSlice2        [10,12,14,16,18]
+
   ]
 
