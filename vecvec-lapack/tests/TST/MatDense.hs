92ce01fd4e94dd0050b8fc5a5278c3d95ed3e9e2
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2023-10-10 10:53:21 +0300

    Add module for tests specific to dense matrices

---
+++ vecvec-lapack/tests/TST/MatDense.hs
@@ -0,0 +1,11 @@
+-- |
+-- Tests for dense matrices
+module TST.MatDense (tests) where
+
+import Test.Tasty
+--import Test.Tasty.QuickCheck
+
+tests :: TestTree
+tests = testGroup "ops.Matrix.Dense"
+  [
+  ]
