71219aacf6aed2a844e388cb5e7dad5e46633bd5
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2023-10-21 08:44:57 +0300

    Some cleanup of tests
     - Use safe variant for all functions. We don't try squeeze more performance so
       extra checks and copying won't hurt.
     - Factor out slice spec to a common definition

--- vecvec-lapack/tests/TST/Memory.hs
+++ vecvec-lapack/tests/TST/Memory.hs
@@ -20,15 +20,16 @@ tests = testGroup "memory"
     [ testCase "miscellaneous intersections" $ do
         let carrierLength = 200
         let sourceOffset  = 30
-        for_ [1..10] $ \sourceLength ->
+        for_ [1..10] $ \len ->
           for_ [-5,-4..5] $ \sourceTargetOffset ->
             for_ [1..10] $ \sourceStride ->
               for_ [1..10] $ \targetStride -> do
-                let targetLength = sourceLength
+                let caseNamePrefix = "(sourceTargetOffset = " ++ show sourceTargetOffset ++ ", "
+                                  ++ "sourceStride = " ++ show sourceStride ++ ", "
+                                  ++ "targetStride = " ++ show targetStride ++ ") "
                 let targetOffset = sourceOffset + sourceTargetOffset
-                let caseNamePrefix =    "(sourceTargetOffset = " ++ show sourceTargetOffset ++ ", "
-                                     ++ "sourceStride = " ++ show sourceStride ++ ", "
-                                     ++ "targetStride = " ++ show targetStride ++ ") "
+                    sliceSrc     = (sourceOffset, Length (len * sourceStride)) `Strided` sourceStride
+                    sliceTgt     = (targetOffset, Length (len * targetStride)) `Strided` targetStride
                 -- According to specification of `move`,
                 -- "If the vectors do not overlap, then this is equivalent to copy.
                 -- Otherwise, the copying is performed as if the source vector were
@@ -38,17 +39,17 @@ tests = testGroup "memory"
                 -- after using the intermediate buffer for copying -- the results should match.
                 let (carrier1', carrier2', target1', target2' :: VV.Vec Int) = runST $ do
                       carrier1    <- MVG.generateM carrierLength pure
-                      let source1 =  slice (Strided (sourceOffset, Length (sourceLength * sourceStride)) sourceStride) carrier1
-                      let target1 =  slice (Strided (targetOffset, Length (targetLength * targetStride)) targetStride) carrier1
-                      MVG.basicUnsafeMove target1 source1
+                      let source1 = slice sliceSrc carrier1
+                      let target1 = slice sliceTgt carrier1
+                      MVG.move target1 source1
                       --
                       carrier2    <- MVG.generateM carrierLength pure
-                      let source2 =  slice (Strided (sourceOffset, Length (sourceLength * sourceStride)) sourceStride) carrier2
-                      let target2 =  slice (Strided (targetOffset, Length (targetLength * targetStride)) targetStride) carrier2
+                      let source2 = slice sliceSrc carrier2
+                      let target2 = slice sliceTgt carrier2
                       tmp <- MVG.clone source2
-                      MVG.basicUnsafeCopy target2 tmp
+                      MVG.copy target2 tmp
                       --
-                      (,,,) <$> VG.unsafeFreeze target1 <*> VG.unsafeFreeze target2 <*> VG.unsafeFreeze carrier1 <*> VG.unsafeFreeze carrier2
+                      (,,,) <$> VG.freeze target1 <*> VG.freeze target2 <*> VG.freeze carrier1 <*> VG.freeze carrier2
                 assertEqual (caseNamePrefix ++  "Carrier vectors should be the same") carrier1' carrier2'
                 assertEqual (caseNamePrefix ++  "Target vectors should be the same")  target1'  target2'
       ]
