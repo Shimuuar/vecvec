e162e32aaf0cda55648159afb4bc0baef95e8a7f
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2023-10-22 17:34:27 +0300

    Clean up test cases for move some more

--- vecvec-lapack/tests/TST/Memory.hs
+++ vecvec-lapack/tests/TST/Memory.hs
@@ -1,17 +1,17 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TypeApplications    #-}
 module TST.Memory (tests) where
 
+import Data.Foldable                         (for_)
+import Data.Vector.Generic                   qualified as VG
+import Data.Vector.Generic.Mutable           qualified as MVG
 
-import           Control.Monad.ST
-import           Data.Foldable                         (for_)
-import qualified Data.Vector.Generic                   as VG
-import qualified Data.Vector.Generic.Mutable           as MVG
+import Vecvec.Classes.NDArray
+import Vecvec.LAPACK                         (Strided(..))
+import Vecvec.LAPACK.Vector.Mutable          (MVec)
 
-import           Vecvec.Classes.NDArray                (Length (..), slice)
-import qualified Vecvec.LAPACK                         as VV
-import           Vecvec.LAPACK.Internal.Vector.Mutable
-
-import           Test.Tasty
-import           Test.Tasty.HUnit
+import Test.Tasty
+import Test.Tasty.HUnit
 
 
 tests :: TestTree
@@ -21,37 +21,41 @@ tests = testGroup "memory"
         let carrierLength = 200
         let sourceOffset  = 30
         for_ [1..10] $ \len ->
-          for_ [-5,-4..5] $ \sourceTargetOffset ->
+          for_ [-5,-4..5] $ \commonOffset ->
             for_ [1..10] $ \sourceStride ->
               for_ [1..10] $ \targetStride -> do
-                let caseNamePrefix = "(sourceTargetOffset = " ++ show sourceTargetOffset ++ ", "
-                                  ++ "sourceStride = " ++ show sourceStride ++ ", "
-                                  ++ "targetStride = " ++ show targetStride ++ ") "
-                let targetOffset = sourceOffset + sourceTargetOffset
+                let targetOffset = sourceOffset + commonOffset
                     sliceSrc     = (sourceOffset, Length (len * sourceStride)) `Strided` sourceStride
                     sliceTgt     = (targetOffset, Length (len * targetStride)) `Strided` targetStride
-                -- According to specification of `move`,
-                -- "If the vectors do not overlap, then this is equivalent to copy.
+                let caseNamePrefix = unlines
+                      [ "sourceOffset = " ++ show sourceOffset
+                      , "targetOffset = " ++ show targetOffset
+                      , "sourceStride = " ++ show sourceStride
+                      , "targetStride = " ++ show targetStride
+                      , "length       = " ++ show len
+                      ]
+                -- move specification:
+                --
+                -- If the vectors do not overlap, then this is equivalent to copy.
                 -- Otherwise, the copying is performed as if the source vector were
                 -- copied to a temporary vector and then the temporary vector was
-                -- copied to the target vector."
-                -- So for the test, we compare the result after using `basicUnafeMove` and
-                -- after using the intermediate buffer for copying -- the results should match.
-                let (carrier1', carrier2', target1', target2' :: VV.Vec Int) = runST $ do
-                      carrier1    <- MVG.generateM carrierLength pure
-                      let source1 = slice sliceSrc carrier1
-                      let target1 = slice sliceTgt carrier1
-                      MVG.move target1 source1
-                      --
-                      carrier2    <- MVG.generateM carrierLength pure
-                      let source2 = slice sliceSrc carrier2
-                      let target2 = slice sliceTgt carrier2
-                      tmp <- MVG.clone source2
-                      MVG.copy target2 tmp
-                      --
-                      (,,,) <$> VG.freeze target1 <*> VG.freeze target2 <*> VG.freeze carrier1 <*> VG.freeze carrier2
-                assertEqual (caseNamePrefix ++  "Carrier vectors should be the same") carrier1' carrier2'
-                assertEqual (caseNamePrefix ++  "Target vectors should be the same")  target1'  target2'
+                -- copied to the target vector.
+                carrier <- MVG.generate @_ @MVec carrierLength id
+                let source = slice sliceSrc carrier
+                    target = slice sliceTgt carrier
+                MVG.move target source
+                -- Reference implementation using temporary
+                carrierRef <- MVG.generate @_ @MVec carrierLength id
+                let sourceRef = slice sliceSrc carrierRef
+                    targetRef = slice sliceTgt carrierRef
+                MVG.copy targetRef =<< MVG.clone sourceRef
+                -- Check for equivality
+                do impl <- VG.freeze target
+                   ref  <- VG.freeze targetRef
+                   assertEqual (caseNamePrefix ++  "Target vectors should be the same") ref impl
+                --
+                do impl <- VG.freeze carrier
+                   ref  <- VG.freeze carrierRef
+                   assertEqual (caseNamePrefix ++  "Carrier vectors should be the same") ref impl
       ]
   ]
-
