bc1cb1f8c3665b6764bb60fc0f932bf0453b14b3
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2024-01-06 22:53:20 +0300

    Extend loops (#18)
    Those are are mostly needed for working with symmetric matrices

--- vecvec-lapack/Vecvec/LAPACK/Utils.hs
+++ vecvec-lapack/Vecvec/LAPACK/Utils.hs
@@ -2,14 +2,41 @@
 -- Utilities for wrinting code
 module Vecvec.LAPACK.Utils
   ( -- * Loops
-    loop0_
+    loop_
+  , loop0_
+  , loopUp_
   ) where
 
+-- | Loop over range @[k,n)@.
+loop_
+  :: (Applicative m)
+  => Int          -- ^ @k@
+  -> Int          -- ^ @n@
+  -> (Int -> m a) -- ^ Callback
+  -> m ()
+{-# INLINE loop_ #-}
+loop_ i0 n action = go i0 where
+  go i | i >= n    = pure ()
+       | otherwise = action i *> go (i+1)
 
--- | Loop over range @[0,n)@. 
-loop0_ :: (Applicative m) => Int -> (Int -> m a) -> m ()
+
+-- | Loop over range @[0,n)@.
+loop0_
+  :: (Applicative m)
+  => Int          -- ^ @n@
+  -> (Int -> m a) -- ^ Callback
+  -> m ()
 {-# INLINE loop0_ #-}
 loop0_ n action = go 0 where
   go i | i >= n    = pure ()
        | otherwise = action i *> go (i+1)
 
+-- | Loop over square matrix of size @n@ which references only
+--   elements on diagonal and above
+loopUp_
+  :: (Applicative m)
+  => Int                 -- ^ Matrix size @n@
+  -> (Int -> Int -> m a) -- ^ Callback \(N_{row}\) \(N_{column}\)
+  -> m ()
+{-# INLINE loopUp_ #-}
+loopUp_ n action = loop0_ n $ \i -> loop_ i n $ \j -> action i j
